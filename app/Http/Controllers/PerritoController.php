8fed08d85b4eab0293ddc37229abaaad3f8a3b87
carlos <carlos.20499@gmail.com>
2021-04-03 01:13:54 -0300

    fix

--- app/Http/Controllers/PerritoController.php
+++ app/Http/Controllers/PerritoController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Perrito;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 use SebastianBergmann\Environment\Console;
 
 class PerritoController extends Controller
@@ -12,31 +15,47 @@ class PerritoController extends Controller
     public function getDogs (){
 
         $perritos = Perrito::all();
-
         return response()->json(['data' => $perritos],200);
     }
 
     public function createDog(Request $request){
 
-        $rules= [
+        $validator = Validator::make($request->all(),
+        [
             'nombre' => 'required',
             'color' => 'required',
             'raza' => 'required',
-        ];
+        ]);
 
-        $quotation = [
+        if($validator->fails()){
+            return response()->json($validator->messages(), 400);
+        }
+
+        $buscarPerrito = Perrito::where('nombre',$request->nombre)->count();
+        if($buscarPerrito != 0 ){
+            return response()->json(['message'=> "ya existe un perrito con ese nombre", "success"=> false], 200);
+        }
+        $perrito = [
             'nombre' => $request->nombre,
-             'color' => $request->color,
-             'raza' => $request->raza,
-         ];
-
-         $this->validate($request, $rules);
-        // dd($request);
-        $dogCreated = Perrito::create($quotation);
-        return response()->json(['data'=> $dogCreated], 200);
+            'color' => $request->color,
+            'raza' => $request->raza,
+        ];
+        $perritoCreado = Perrito::create($perrito);
+        return response()->json(['data'=> $perritoCreado], 200);
     }
 
     public function updateDog(Request $request, $id){
+
+        $validator = Validator::make($request->all(),
+        [
+            'nombre' => 'required|unique:perritos',
+            'color' => 'required',
+            'raza' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->messages(), 400);
+        }
 
         $perrito = Perrito::find($id);
 
@@ -59,21 +78,4 @@ class PerritoController extends Controller
         $perrito->delete();
         return response()->json(['data'=> $perrito], 200);
     }
-
-
-
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Perrito  $perrito
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Perrito $perrito)
-    {
-        //
-    }
-
-
 }
